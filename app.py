e8579b064a7cfc0dbfb960635ed350cd941fd32f
Toby Gaskell <tobygaskell@tobys-mbp.home>
2022-11-01 17:27:42 +0000

    added in the last years result

--- app.py
+++ app.py
@@ -7,65 +7,92 @@ import utils as utils
 st.sidebar.image('5.png', width = 100)
 
 player_id = utils.check_if_player(st.experimental_user['email'])
+
 st.title('Football Prediction Game')
+
 st.write('Player ID - {}'.format(player_id))
+
+st.markdown('---')
 
 round = utils.get_api("https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds", {"league":"39","season":"2022","current":"true"})[0][-2:].strip()
 
 teams = utils.run_static_query('SELECT team_name, logo, team_id FROM teams order by team_name asc;')
 
-st.sidebar.markdown('## Pick a Team')   
-option = st.sidebar.radio('Pick a team', teams['TEAM_NAME'], label_visibility = 'collapsed')
+query = '''
+        SELECT HOME_TEAM, AWAY_TEAM, dayname(kickoff)|| ' ' || TO_VARCHAR(KICKOFF, 'HH12 AM') as game_day
+        FROM FIXTURES
+        WHERE ROUND = {}
+        AND SEASON = 2022
+        '''.format(round)
 
+fixtures = utils.run_static_query(query)
+
+body = '## Fixtures this round:'
+for index, row in fixtures.iterrows(): 
+    body = body + '\n- {} vs {}'.format(row['HOME_TEAM'], row['AWAY_TEAM'])
+
+st.markdown(body)
+
+st.markdown('## Pick a Team')   
+
+option = st.selectbox('Pick a team', teams['TEAM_NAME'], label_visibility = 'collapsed')
 option_id = teams[teams['TEAM_NAME'] == option].reset_index(drop = True)['TEAM_ID'][0]
 
-left, right = st.columns(2)
+st.markdown('---')
 
-with left: 
+st.image(teams[teams['TEAM_ID'] == option_id].reset_index(drop = True)['LOGO'][0])
 
-    query = '''
-            SELECT HOME_TEAM, AWAY_TEAM, dayname(kickoff)|| ' ' || TO_VARCHAR(KICKOFF, 'HH12 AM') as game_day
-            FROM FIXTURES
-            WHERE ROUND = {}
-            '''.format(round)
+st.markdown('## {}'.format(option))
+if option in fixtures['HOME_TEAM'].unique(): 
+    st.markdown('##### Home to {} ({})'.format(fixtures[fixtures['HOME_TEAM']== option].reset_index(drop = True)['AWAY_TEAM'][0], 
+                                            fixtures[fixtures['HOME_TEAM']== option].reset_index(drop = True)['GAME_DAY'][0]))
 
-    fixtures = utils.run_static_query(query)
+elif option in fixtures['AWAY_TEAM'].unique(): 
+    st.markdown('##### Away to {} ({})'.format(fixtures[fixtures['AWAY_TEAM']== option].reset_index(drop = True)['HOME_TEAM'][0], 
+                                            fixtures[fixtures['AWAY_TEAM']== option].reset_index(drop = True)['GAME_DAY'][0]))
 
-    body = '## Fixtures this round:'
-    for index, row in fixtures.iterrows(): 
-        body = body + '\n- {} vs {}'.format(row['HOME_TEAM'], row['AWAY_TEAM'])
+st.markdown('#### Last 5 Results:')
 
-    st.markdown(body)
+query = '''
+        select round,home_team || ' vs ' || away_team as game, 
+                score, 
+                case when (home_team = '{option}' and home_goals > away_goals)
+                        or (away_team = '{option}' and away_goals > home_goals) 
+                    then 'Win' 
+                    when home_goals = away_goals 
+                    then 'Draw' 
+                    when (home_team = '{option}' and home_goals < away_goals)
+                        or (away_team = '{option}' and away_goals < home_goals) 
+                    then 'Loss' end as result 
+        from results as a
+        inner join fixtures as b 
+        on a.game_id = b.id
+        where home_team || away_team like '%{option}%'
+        and game_status = 'FT'
+        AND SEASON = 2022
+        order by kickoff desc 
+        limit 5 
+        '''.format(option = option)
 
-with right: 
-    st.image(teams[teams['TEAM_ID'] == option_id].reset_index(drop = True)['LOGO'][0])
+def colouring(val): 
 
-    if option in fixtures['HOME_TEAM'].unique(): 
-        st.markdown('## {} are at home to {} ({})'.format(option, 
-                                                         fixtures[fixtures['HOME_TEAM']== option].reset_index(drop = True)['AWAY_TEAM'][0], 
-                                                         fixtures[fixtures['HOME_TEAM']== option].reset_index(drop = True)['GAME_DAY'][0]))
-
-    elif option in fixtures['AWAY_TEAM'].unique(): 
-        st.markdown('## {} are away to {} ({})'.format(option, 
-                                                       fixtures[fixtures['AWAY_TEAM']== option].reset_index(drop = True)['HOME_TEAM'][0], 
-                                                       fixtures[fixtures['AWAY_TEAM']== option].reset_index(drop = True)['GAME_DAY'][0]))
-
-
+    if val['RESULT'] == 'Win': 
+        colour = 'background-color: #87c46a' 
+    elif val['RESULT'] == 'Draw': 
+        colour = 'background-color: #e6c050' 
+    elif val['RESULT'] == 'Loss': 
+        colour =  'background-color: #d6796d'
 
     
-    # st.markdown('#### From This Season:')
-    # form = utils.get_api("https://api-football-v1.p.rapidapi.com/v3/teams/statistics", {"league":"39",
-    #                                                                                     "season":"2022", 
-    #                                                                                     "team":option_id})['form']
-    # st.write(form)
+    return [colour] * 4
 
+st.dataframe(utils.run_static_query(query).style.apply(colouring, axis = 1))
 
+st.markdown('#### Result for this Fixture Last Season:')
 
-    st.markdown('#### last 5 Results:')
+if option in fixtures['HOME_TEAM'].unique(): 
     query = '''
-            select round,home_team || ' vs ' || away_team as game, 
-                   score, 
-                   case when (home_team = '{option}' and home_goals > away_goals)
+            SELECT SCORE, case when (home_team = '{option}' and home_goals > away_goals)
                             or (away_team = '{option}' and away_goals > home_goals) 
                         then 'Win' 
                         when home_goals = away_goals 
@@ -73,39 +100,49 @@ with right:
                         when (home_team = '{option}' and home_goals < away_goals)
                             or (away_team = '{option}' and away_goals < home_goals) 
                         then 'Loss' end as result 
-            from results as a
-            inner join fixtures as b 
-            on a.game_id = b.id
-            where home_team || away_team like '%{option}%'
-            and game_status = 'FT'
-            order by kickoff desc 
-            limit 5 
-            '''.format(option = option)
+            FROM fixtures 
+            INNER JOIN results 
+            ON id = game_id 
+            WHERE home_team = '{option}' 
+            AND away_team = '{oppo}' 
+            AND season = 2021 
+            '''.format(option = option, oppo = fixtures[fixtures['HOME_TEAM']== option].reset_index(drop = True)['AWAY_TEAM'][0])
 
-    # st.write('WWWWW')
+if option in fixtures['AWAY_TEAM'].unique(): 
+    query = '''
+            SELECT SCORE, case when (home_team = '{option}' and home_goals > away_goals)
+                            or (away_team = '{option}' and away_goals > home_goals) 
+                        then 'Win' 
+                        when home_goals = away_goals 
+                        then 'Draw' 
+                        when (home_team = '{option}' and home_goals < away_goals)
+                            or (away_team = '{option}' and away_goals < home_goals) 
+                        then 'Loss' end as result 
+            FROM fixtures 
+            INNER JOIN results 
+            ON id = game_id 
+            WHERE home_team = '{oppo}' 
+            AND away_team = '{option}' 
+            AND season = 2021 
+            '''.format(option = option, oppo = fixtures[fixtures['AWAY_TEAM']== option].reset_index(drop = True)['HOME_TEAM'][0])
 
-    def colouring(val): 
+last_year_result  = utils.run_static_query(query)
 
-        if val['RESULT'] == 'Win': 
-            colour = 'background-color: #87c46a' 
-        elif val['RESULT'] == 'Draw': 
-            colour = 'background-color: #e6c050' 
-        elif val['RESULT'] == 'Loss': 
-            colour =  'background-color: #d6796d'
+if len(last_year_result) > 0: 
+    if last_year_result['RESULT'][0] == 'Win': 
+        st.markdown('##### {} {} ✅'.format(last_year_result['SCORE'][0], last_year_result['RESULT'][0]))
+    elif last_year_result['RESULT'][0] == 'Loss': 
+        st.markdown('##### {} {} ❌'.format(last_year_result['SCORE'][0], last_year_result['RESULT'][0]))
+    elif last_year_result['RESULT'][0] == 'Draw': 
+        st.markdown('##### {} {} 🤝'.format(last_year_result['SCORE'][0], last_year_result['RESULT'][0]))    
+else:
+    st.markdown('This fixture didn\'t happen last year')
 
-        
-        return [colour] * 4
-
-    st.dataframe(utils.run_static_query(query).style.apply(colouring, axis = 1))
-
-    # .applymap(lambda x: "background-color: red" if x>0 else "background-color: white")
-
-
-# df.style.applymap(color_survived, subset=['Survived'])
+st.markdown('---')
 
 
 
-if st.sidebar.button('Submit Choice'): 
+if st.button('Submit Choice'): 
 
     exists = utils.check_if_submitted(player_id, round)
 
@@ -124,6 +161,7 @@ if st.sidebar.button('Submit Choice'):
                 '''.format(option, player_id, round)
 
     utils.run_query(query)
+    st.write('Choice Submitted')
 
 
 
