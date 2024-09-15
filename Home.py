215387d2eef4369a54e72938f4681a5877aa3cd0
Toby Gaskell <Toby96@sky.com>
2024-09-16 00:15:59 +0100

    start again

--- Home.py
+++ Home.py
@@ -1,61 +1,63 @@
 import streamlit as st 
-import utils
-from streamlit_extras.switch_page_button import switch_page
+# import utils
+# from streamlit_extras.switch_page_button import switch_page
 
 
-# st.set_page_config(layout="wide")
+# # st.set_page_config(layout="wide")
 
-st.image('5.png', width = 100)
+# st.image('5.png', width = 100)
 
 
-round = utils.get_api("https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds", 
-                     {"league":"39","season":"2024","current":"true"})[0][-2:].strip()
+# round = utils.get_api("https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds", 
+#                      {"league":"39","season":"2024","current":"true"})[0][-2:].strip()
 
-player_id = utils.check_if_player(st.experimental_user['email'])
+# player_id = utils.check_if_player(st.experimental_user['email'])
+
+round = 'Testing'
 
 st.markdown('# Welcome to FPG - Round {}'.format(round))
 
 
-query = '''
-        SELECT double_point_round, draw_round, 
-               dayname(cut_off) || ' ' || day(cut_off) || ' at ' || TO_VARCHAR(cut_off, 'HH12AM') as cut_off 
-        FROM rounds 
-        where round = {}
-        '''.format(round)
+# query = '''
+#         SELECT double_point_round, draw_round, 
+#                dayname(cut_off) || ' ' || day(cut_off) || ' at ' || TO_VARCHAR(cut_off, 'HH12AM') as cut_off 
+#         FROM rounds 
+#         where round = {}
+#         '''.format(round)
 
-round_info = utils.run_static_query(query)
+# round_info = utils.run_static_query(query)
 
-# st.header('hello world')
+# # st.header('hello world')
 
-double_round = round_info['DOUBLE_POINT_ROUND'][0]
+# double_round = round_info['DOUBLE_POINT_ROUND'][0]
 
-draw_round = round_info['DRAW_ROUND'][0]
+# draw_round = round_info['DRAW_ROUND'][0]
 
-cutoff = round_info['CUT_OFF'][0]
+# cutoff = round_info['CUT_OFF'][0]
 
-st.subheader('🔘 DeadLine 💀: {}'.format(cutoff))
+# st.subheader('🔘 DeadLine 💀: {}'.format(cutoff))
 
-st.subheader('🔘 Double Points Round {}'.format('✅' if double_round else '❌' ))
+# st.subheader('🔘 Double Points Round {}'.format('✅' if double_round else '❌' ))
 
-st.subheader('🔘 Draw Round {}'.format('✅' if draw_round else '❌' ))
-
-
+# st.subheader('🔘 Draw Round {}'.format('✅' if draw_round else '❌' ))
 
 
 
-st.markdown('---')
 
-left, right = st.columns(2)
 
-with left:
-    if st.button('Pick Team'):
+# st.markdown('---')
 
-        switch_page("make choice")
+# left, right = st.columns(2)
 
-with right: 
-    if st.button('Check Standing'):
+# with left:
+#     if st.button('Pick Team'):
 
-        switch_page("standings")
+#         switch_page("make choice")
+
+# with right: 
+#     if st.button('Check Standing'):
+
+#         switch_page("standings")
 
 
 
