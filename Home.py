99e0d91346f709c6cd5dfdd5245512c2a96b11a4
Toby Gaskell <Toby96@sky.com>
2024-09-21 23:49:39 +0100

    working version

--- Home.py
+++ Home.py
@@ -1,144 +1,85 @@
 import streamlit as st 
-# import utils
-# from streamlit_extras.switch_page_button import switch_page
-import requests as r 
-import random 
 import Rules as rules
 import pandas as pd
-# # st.set_page_config(layout="wide")
+import utils
+import streamlit.components.v1 as components
 
-# st.image('5.png', width = 100)
 
-
-# round = utils.get_api("https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds", 
-#                      {"league":"39","season":"2024","current":"true"})[0][-2:].strip()
-
-# player_id = utils.check_if_player(st.experimental_user['email'])
+# st.set_page_config(layout="wide")
 
 if 'page_view' not in st.session_state: 
     st.session_state['page_view'] = 'Make Choice'
 
-env = 'Not Local'
-player = 100
-round = random.choice([i+1 for i in range(3)])
+round = utils.fpg_api_static('current_round')['Round ID']
 
+round_data = utils.fpg_api('get_round_info', {'Round': round})
 
+round_def = 'Normal Round'
 
+if round_data['DMM']: 
+    round_def = 'Draw Means More Round!'
 
+if round_data['Double']: 
+    round_def = 'Double Points Round!'
 
+if round_data['DMM'] and round_data['Double']:
+    round_def = 'Draw Means Mores AND Double Points Round!!'
 
-if env == 'Local':
-    url = 'http://192.168.0.110:5001'
-else: 
-    url = 'http://94.2.195.17:5001'
+data = {'Email': st.experimental_user.email}
 
-
-data = {'Email':st.experimental_user.email}
-
-player_id = r.post(url + '/init_player', json = data).json()['player_id']
-
-
-# st.sidebar.caption()
+player_id = utils.fpg_api('init_player', data)['player_id']
 
 if st.sidebar.button('Rules', use_container_width=True): 
     rules.view_rules()
-# round = 'Testing'
-
-
 
 st.markdown('<h1 style="text-align: center;"> FPG </h1>', unsafe_allow_html=True)
-st.markdown('<h3 style="text-align: center;"> Round {} </h3>'.format(round), unsafe_allow_html=True)
+st.markdown('<h3 style="text-align: center;"> Round {} - {} </h3>'.format(round, round_def), unsafe_allow_html=True)
 
 st.markdown('---')
-
-# round_info = r.post(url + '/get_round_info', json = {'Round' : round}).json()
-
-# st.write(round_info)
 
 left, right = st.columns(2)
 
 if left.button('Make Choice', use_container_width=True): 
     st.session_state['page_view'] = 'Make Choice'
 
-if right.button('See Standings', use_container_width=True): 
+if right.button('See FPG Table', use_container_width=True): 
     st.session_state['page_view'] = 'See Standings'
 
 st.markdown(' ')
-st.markdown(' ')
-st.markdown(' ')
 
-# with left: 
+# st.sidebar.button(round_def, use_container_width=True)
+
 st.sidebar.markdown('---')
-# st.sidebar.button('Draw Means More Round', disabled = not round_info['DMM'], use_container_width= True)
-st.sidebar.button('Draw Means More Round', disabled = False, use_container_width= True)
-# st.sidebar.write(round_info['DMM'])
 
-# with right: 
-# st.sidebar.button('Double Round', disabled = not round_info['Double'], use_container_width=True )
-st.sidebar.button('Double Round', disabled = True, use_container_width = True)
-# st.sidebar.write(round_info['Double'])
 st.sidebar.caption('Email: {}'.format(st.experimental_user.email))
 st.sidebar.caption('Player ID: {}'.format(player_id))
 
-
 if st.session_state['page_view'] == 'Make Choice':
 
-    fixtures = pd.DataFrame({'Home Team':['Team 1',
-                                        'Team 2',
-                                        'Team 3',
-                                        'Team 4',
-                                        'Team 5',
-                                        'Team 6',
-                                        'Team 7',
-                                        'Team 8',
-                                        'Team 9', 
-                                        'Team 10'], 
-                            'Away Team':['Team 11',
-                                        'Team 12',
-                                        'Team 13',
-                                        'Team 14',
-                                        'Team 15',
-                                        'Team 16',
-                                        'Team 17',
-                                        'Team 18',
-                                        'Team 19', 
-                                        'Team 20']})
+    data = {'Round': round}
 
+    fixtures = utils.fpg_api('get_fixtures', data)
 
+    fix=pd.DataFrame(fixtures)
 
-    # def left_align(s):
-    #     return 'text-align: center;'
+    fix['vs'] = 'vs'
 
+    fix['Fixtures'] = fix['HOME_TEAM'] + ' ' + fix['vs'] + ' ' + fix['AWAY_TEAM']
 
+    fix.rename(columns = {'HOME_LOGO' : ' ', 
+                'AWAY_LOGO': '  '}, inplace = True)
+    
+    st.dataframe(fix[[' ', 'Fixtures', '  ']],column_config={
+        " ": st.column_config.ImageColumn(), 
+        "  ":st.column_config.ImageColumn()}, 
+        use_container_width= True, hide_index=True)
 
-    # st.dataframe(fixtures.style.applymap(left_align), use_container_width=True, hide_index=True)
-    left, center, right = st.columns(3)
+    data = {'Player': player_id}
 
-    left.caption('Home Team')
+    teams = utils.fpg_api('get_available_choices', data)
 
-    right.caption('<div style="text-align: right"> Away Team </div>', unsafe_allow_html=True)
-    with st.container(border=True):
-
-
-        for index, row in fixtures.iterrows():
-            left, center, right  = st.columns(3)
-
-            left.write(row['Home Team'])
-
-            center.caption('<div style="text-align: center"> vs </div>', unsafe_allow_html=True)
-
-            right.write('<div style="text-align: right"> {} </div>'.format(row['Away Team']), unsafe_allow_html=True)
-
-    # st.dataframe(fixtures, use_container_width=True, hide_index=True)
-
-
-    teams = r.get(url = url + '/get_teams').json()['Teams']
-
-
-    with st.form('Choice', border = False):
-        team_choice = st.selectbox('Pick a Team:', teams )
-
-        # round_choice = st.number_input('Pick a Round:', step = 1, value= 1, max_value= 38 )
+    with st.form('Choice', border = True):
+        team_choice = st.selectbox('Pick a Team:', [team['TEAM_NAME'] for team in  teams] )
 
         if st.form_submit_button('Submit', use_container_width=True): 
 
@@ -146,37 +87,12 @@ if st.session_state['page_view'] == 'Make Choice':
                     'Choice' : team_choice, 
                     'Round'  : round}
             
-            r.post(url + '/make_choice', json = data)
+            submitted = utils.fpg_api('make_choice', data)
 
-            data = {'Player': player_id, 
-                    'Choice': team_choice, 
-                    'Round': round}
-            
-            result_data = r.post(url + '/get_result', json = data).json()
-
-            # data = {'Player' : player, 
-            #         'Result' : result_data['Result'], 
-            #         'H2H' : result_data['H2H'], 
-            #         'Derby': result_data['Derby'], 
-            #         'DMM': round_info['DMM'], 
-            #         'Doubled': round_info['Double']}
-            
-            data = {'Player' : player_id, 
-                    'Result' : result_data['Result'], 
-                    'H2H' : result_data['H2H'], 
-                    'Derby': result_data['Derby'], 
-                    'DMM': True, 
-                    'Doubled': False}
-            
-            score_data = r.post(url + '/get_score', json = data).json()
-
-            left, right = st.columns(2)
-
-            with left: 
-                st.write(result_data)
-
-            with right: 
-                st.write(score_data)
+            if submitted['Submitted']: 
+                st.success('You have submitted {} as you choice for round {} - Thankyou for playing!'.format(team_choice, round))
+            else: 
+                st.error('There was an issue submitting your choicer')
 
 if st.session_state['page_view'] == 'See Standings':
 
--- utils.py
+++ utils.py
@@ -1,98 +1,29 @@
 import streamlit as st 
-import requests as re 
-import pandas as pd 
-import snowflake.connector
+import requests as r
 
 
-# Initialize connection.
-# Uses st.experimental_singleton to only run once.
-# @st.experimental_singleton
-def init_connection():
-    return snowflake.connector.connect(
-        **st.secrets["snowflake"], client_session_keep_alive=True
-    )
-
-conn = init_connection()
-
-# Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-# @st.experimental_memo(ttl=600)
-def run_static_query(query):
+def fpg_api(endpoint, data = None):
     '''
     '''
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return pd.DataFrame.from_records(iter(cur), columns = [x[0] for x in cur.description])
+    url = st.secrets['fpg']['host']
 
-def run_query(query):
+    if data == None:
+        response = r.get(url + endpoint).json()
+
+    else:
+        response = r.post(url + endpoint, json = data).json()
+        # st.write(response)
+    return response
+
+@st.cache_data(ttl = 600)
+def fpg_api_static(endpoint, data = None):
     '''
     '''
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return pd.DataFrame.from_records(iter(cur), columns = [x[0] for x in cur.description])
+    url = st.secrets['fpg']['host']
 
+    if data == None:
+        response = r.get(url + endpoint).json()
 
-# @st.experimental_singleton
-def get_api(url, querystring = {}): 
-    '''
-    '''
-    headers = {
-        "X-RapidAPI-Key": st.secrets["api"]['key'],
-        "X-RapidAPI-Host": st.secrets["api"]['host']
-    }
-
-    response = re.request("GET", url, headers=headers, params=querystring)
-
-    return response.json()['response']
-
-
-def check_if_submitted(player_id, round_num): 
-     '''
-     '''
-     exists = False
-     query = '''
-             SELECT COUNT(DISTINCT TEAM_CHOICE) AS submitted 
-             FROM choices   
-             WHERE UPPER(PLAYER_ID) = '{}' 
-             AND ROUND = {};
-             '''.format(player_id, round_num)
-
-     if run_query(query)['SUBMITTED'][0] > 0: 
-         exists = True 
-
-     return exists
-
-def get_player_id(email): 
-    '''
-    '''
-    query = '''
-            SELECT PLAYER_ID 
-            FROM PLAYERS 
-            WHERE EMAIL = '{}'
-            '''.format(email)
-
-    return run_static_query(query)['PLAYER_ID'][0]
-
-
-def check_if_player(email): 
-    '''
-    '''
-    exist = False
-
-    query = '''
-            SELECT COUNT(DISTINCT PLAYER_ID) AS PLAYER_EXISTS
-            FROM PLAYERS
-            WHERE EMAIL = '{}'
-            '''.format(email)
-
-    if run_query(query)['PLAYER_EXISTS'][0] == 0: 
-
-        query = '''
-                INSERT INTO PLAYERS
-                VALUES
-                (SEQ_PLAYER_ID.NEXTVAL, '{}')
-                '''.format(email)
-
-        run_query(query)
-
-    return get_player_id(email)
+    else:
+        response = r.post(url + endpoint, json = data).json()
+    return response
