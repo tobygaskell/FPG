578024336611910d5ad44e5bad50ca5177a6869c
Toby Gaskell <Toby96@sky.com>
2024-09-22 19:45:38 +0100

    second iteration of changes

--- Home.py
+++ Home.py
@@ -3,6 +3,7 @@ import Rules as rules
 import pandas as pd
 import utils
 import streamlit.components.v1 as components
+import Dialogs
 
 
 st.set_page_config(layout="wide")
@@ -15,6 +16,7 @@ round = utils.fpg_api_static('current_round')['Round ID']
 round_data = utils.fpg_api('get_round_info', {'Round': round})
 
 round_def = 'Normal Round'
+submitted = False
 
 if round_data['DMM']: 
     round_def = 'Draw Means More Round!'
@@ -27,7 +29,9 @@ if round_data['DMM'] and round_data['Double']:
 
 data = {'Email': st.experimental_user.email}
 
-player_id = utils.fpg_api('init_player', data)['player_id']
+# player_id = utils.fpg_api('init_player', data)['player_id']
+
+player_id = 2
 
 if st.sidebar.button('Rules', use_container_width=True): 
     rules.view_rules()
@@ -61,14 +65,16 @@ if st.session_state['page_view'] == 'Make Choice':
     fixtures = utils.fpg_api('get_fixtures', data)
 
     fix=pd.DataFrame(fixtures)
+    # st.write(fix)
 
-    fix['vs'] = 'vs'
+    fix['vs'] = fix['DERBY'].map(lambda x: '-' if not x else '⚔️')
 
     fix['Fixtures'] = fix['HOME_TEAM'] + ' ' + fix['vs'] + ' ' + fix['AWAY_TEAM']
 
     fix.rename(columns = {'HOME_LOGO' : ' ', 
                 'AWAY_LOGO': '  '}, inplace = True)
     
+    st.caption('⚔️ means this fixture is a derby')
     st.dataframe(fix[[' ', 'Fixtures', '  ']],column_config={
         " ": st.column_config.ImageColumn(), 
         "  ":st.column_config.ImageColumn()}, 
@@ -78,7 +84,7 @@ if st.session_state['page_view'] == 'Make Choice':
 
     teams = utils.fpg_api('get_available_choices', data)
 
-    with st.form('Choice', border = True):
+    with st.form('Choice', border = False):
         team_choice = st.selectbox('Pick a Team:', [team['TEAM_NAME'] for team in  teams] )
 
         if st.form_submit_button('Submit', use_container_width=True): 
@@ -88,15 +94,26 @@ if st.session_state['page_view'] == 'Make Choice':
                     'Round'  : round}
             
             submitted = utils.fpg_api('make_choice', data)
+            # submitted = {}r
+            # submitted['Submitted'] = 'Too Late'
 
-            if submitted['Submitted']: 
-                st.success('You have submitted {} as you choice for round {} - Thankyou for playing!'.format(team_choice, round))
-            else: 
-                st.error('There was an issue submitting your choicer')
+    if submitted:
+        if submitted['Submitted'] == True: 
+            st.success('You have submitted {} as you choice for round {} - Thankyou for playing!'.format(team_choice, round))
+
+        elif submitted['Submitted'] == 'Already Chosen':
+            Dialogs.update_choice(team_choice, player_id, round)
+
+        elif submitted['Submitted'] == 'Too Late': 
+            st.error('Too Late - It\'s past the cut off time for submitting a choice for round {}! Come back after the games have finished.'.format(round))
+
+        else: 
+            st.error('There was an issue submitting your choicer')
 
 if st.session_state['page_view'] == 'See Standings':
+    
+    stand = utils.fpg_api('get_standings')
+    
+    standings = pd.DataFrame(stand)
 
-    standings = pd.DataFrame({'Player':['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5', 'Player 6'], 
-                            'Score': [34, 33, 29, 25, 0, -10]})
-
-    st.dataframe(standings, use_container_width=True, hide_index=True)
+    st.dataframe(standings[['USER', 'SCORE']], use_container_width=True, hide_index=True)
